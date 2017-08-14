d1d5ea454ae8d07781724400565d03334bb0c902
Mark Angelo Libantino <markangelol@google.com>
2017-08-14 16:14:36 +0800

    Add custom locale

---
+++ src/timeago/locales/custom.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Created on 2017-8-14
+
+@author: digyter
+'''
+
+LOCALE = [
+    ["1 sec", "a while"],
+    ["%s sec", "in %s seconds"],
+    ["1 min", "in 1 minute"],
+    ["%s min", "in %s minutes"],
+    ["1 hr", "in 1 hour"],
+    ["%s hr", "in %s hours"],
+    ["1 d", "in 1 day"],
+    ["%s d", "in %s days"],
+    ["1 wk", "in 1 week"],
+    ["%s wk", "in %s weeks"],
+    ["1 m", "in 1 month"],
+    ["%s m", "in %s months"],
+    ["1 yr", "in 1 year"],
+    ["%s yr", "in %s years"],
+]
